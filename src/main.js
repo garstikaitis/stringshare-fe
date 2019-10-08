26587f508f6beb2c677b1edcc99cce1faf00fa52
Gintaras Arštikaitis <garstikaitis@gmail.com>
2019-10-08 20:41:02 +0200

    render graphql

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://stringshare.test/graphql',
+  // fetchOptions: {
+  //   mode: 'cors',
+  // },
+});
+
+Vue.use(VueApollo);
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-for="user in users" :key="user.id">{{ user.email }}</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import HelloWorld from "@/components/HelloWorld.vue";
+import gql from "graphql-tag";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    users: gql`
+      query {
+        users {
+          id
+          email
+        }
+      }
+    `
   },
+  data() {
+    return {
+      users: null
+    };
+  },
+  name: "home",
+  components: {
+    HelloWorld
+  }
 };
 </script>
